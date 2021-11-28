b4eeb450360b99c696b7bcdb5d172afec7644e53
Midi Nanakul <midinanakul@hotmail.com>
2021-11-27 22:49:02 -0800

    Moved everything into a class.

--- expense_tracker.py
+++ expense_tracker.py
@@ -6,16 +6,34 @@ import pandas as pd
 import sys
 
 
+class MyWindow(QMainWindow):
+    def __init__(self):
+        super(MyWindow, self).__init__()
+        self.label = QtWidgets.QLabel(self)
+        self.weekly_expense_button = QtWidgets.QPushButton(self)
+        self.setGeometry(300, 300, 300, 300)
+        self.setWindowTitle('Expense Tracker')
+        self.initialize_ui()
+
+    def initialize_ui(self):
+        self.label.setText('Not clicked.')
+        self.label.move(50, 50)
+
+        self.weekly_expense_button.setText('Weekly Expenses')
+        self.weekly_expense_button.setGeometry(0, 0, 130, 50)
+        self.weekly_expense_button.clicked.connect(self.weekly_expense_button_click)
+
+    def weekly_expense_button_click(self):
+        self.label.setText('You have clicked the button!')
+        self.update()
+
+    def update(self):
+        self.label.adjustSize()
+
+
 def window():
     app = QApplication(sys.argv)
-    win = QMainWindow()
-    win.setGeometry(300, 300, 300, 300)
-    win.setWindowTitle('Expense Tracker')
-
-    weekly_expense_button = QtWidgets.QPushButton(win)
-    weekly_expense_button.setText('Weekly Expenses')
-    weekly_expense_button.setGeometry(0, 0, 130, 50)
-
+    win = MyWindow()
     win.show()
     sys.exit(app.exec_())
 
