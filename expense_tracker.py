33686020d009b413a3681e4258489874abd37dff
Midi Nanakul <midinanakul@hotmail.com>
2021-11-28 17:11:32 -0800

    Commented out code. Working on logic first before implementing SQLite3 and a GUI.

--- expense_tracker.py
+++ expense_tracker.py
@@ -1,3 +1,4 @@
+import sqlite3
 from PyQt5 import QtWidgets
 from PyQt5.QtWidgets import QApplication, QMainWindow
 from datetime import datetime
@@ -6,36 +7,73 @@ import pandas as pd
 import sys
 
 
-class MyWindow(QMainWindow):
-    def __init__(self):
-        super(MyWindow, self).__init__()
-        self.label = QtWidgets.QLabel(self)
-        self.weekly_expense_button = QtWidgets.QPushButton(self)
-        self.setGeometry(300, 300, 300, 300)
-        self.setWindowTitle('Expense Tracker')
-        self.initialize_ui()
-
-    def initialize_ui(self):
-        self.label.setText('Not clicked.')
-        self.label.move(50, 50)
-
-        self.weekly_expense_button.setText('Weekly Expenses')
-        self.weekly_expense_button.setGeometry(0, 0, 130, 50)
-        self.weekly_expense_button.clicked.connect(self.weekly_expense_button_click)
-
-    def weekly_expense_button_click(self):
-        self.label.setText('You have clicked the button!')
-        self.update()
-
-    def update(self):
-        self.label.adjustSize()
+# Connect the database
+# connect = db.connect('MyExpenses.sqlite')
+# cursor = connect.cursor()
+# cursor.execute("""CREATE TABLE Expenses (
+#                 item VARCHAR,
+#                 date VARCHAR,
+#                 price VARCHAR
+#                 )""")
+# connect.commit()
 
 
-def window():
-    app = QApplication(sys.argv)
-    win = MyWindow()
-    win.show()
-    sys.exit(app.exec_())
+# 1.) Display Date and Time
+# 2.) Ask user for what they bought
+# 3.) Ask user what day they bought it
+# 4.) Ask how much it cost
+# 5.) Save info to database
 
 
-window()
+def new_expense():
+    item = input('What did you purchase? ')
+    date_purchased = input('What day did you make the purchase? Format: MM/DD/YYYY ')
+    item_price = float(input('How much did this cost? '))
+
+    # connect.execute('INSERT INTO Expenses VALUES (:item, :date, :price)',
+    #                 (str({item}), str({date_purchased}), str({item_price})))
+    # connect.commit()
+    # print(cursor.fetchall())
+    # connect.commit()
+    # connect.close()
+
+
+new_expense()
+
+
+# class MyWindow(QMainWindow):
+#     def __init__(self):
+#         super(MyWindow, self).__init__()
+#         self.label = QtWidgets.QLabel(self)
+#         self.weekly_expense_button = QtWidgets.QPushButton(self)
+#         self.setGeometry(300, 300, 300, 300)
+#         self.setWindowTitle('Expense Tracker')
+#         self.initialize_ui()
+#
+#     def initialize_ui(self):
+#         self.label.setText('Not clicked.')
+#         self.label.move(50, 50)
+#
+#         self.weekly_expense_button.setText('Weekly Expenses')
+#         self.weekly_expense_button.setGeometry(0, 0, 130, 50)
+#         self.weekly_expense_button.clicked.connect(self.weekly_expense_button_click)
+#
+#     def weekly_expense_button_click(self):
+#         self.label.setText('You have clicked the button!')
+#         self.update()
+#
+#     def update(self):
+#         self.label.adjustSize()
+
+
+# def window():
+#     app = QApplication(sys.argv)
+#     win = MyWindow()
+#     win.show()
+#     sys.exit(app.exec_())
+
+
+# window()
+
+if __name__ == '__main__':
+    pass
---
+++ expenses.py
@@ -0,0 +1,23 @@
+from datetime import datetime
+
+# 1.) Display Date and Time
+# 2.) Ask user for what they bought
+# 3.) Ask user what day they bought it
+# 4.) Ask how much it cost
+# 5.) Save info to database
+
+date_now = datetime.now().date().strftime('%m-%d-%Y')
+time_now = datetime.now().strftime('%I:%M:%S %p')
+
+
+def welcome():
+    print('++++++++++++++++++++++++++++++')
+    print('-----------WELCOME!-----------')
+    print('DATE: ' + date_now)
+    print('TIME: ' + time_now)
+    print('++++++++++++++++++++++++++++++')
+    choose_option = 
+
+
+if __name__ == '__main__':
+    welcome()
